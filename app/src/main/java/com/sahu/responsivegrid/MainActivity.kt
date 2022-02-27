a8c429fd546a5e264995568296e879584769edcc
sahruday <sahrudayjagarapu@gmail.com>
2022-02-27 16:01:42 +0530

    Overlay of Gird Configuration. (#4)

--- app/src/main/java/com/sahu/responsivegrid/MainActivity.kt
+++ app/src/main/java/com/sahu/responsivegrid/MainActivity.kt
@@ -12,16 +12,22 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Checkbox
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.sahu.gridconfiguration.LocalGridConfiguration
 import com.sahu.gridconfiguration.columnedWidth
+import com.sahu.responsivegrid.gridsystem.ResponsiveGridOverlay
 import com.sahu.responsivegrid.gridsystem.rememberGridConfiguration
 import com.sahu.responsivegrid.ui.theme.ResponsiveGridTheme
 
@@ -30,10 +36,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ResponsiveGridTheme {
-                // A surface container using the 'background' color from the theme
+                var showOverLay by remember { mutableStateOf(false)}
+
                 CompositionLocalProvider(LocalGridConfiguration provides rememberGridConfiguration()) {
-                    Surface(color = MaterialTheme.colors.background) {
-                        GridColumns(modifier = Modifier.padding(horizontal = LocalGridConfiguration.current.horizontalMargin))
+                    ResponsiveGridOverlay(showOverLay) {
+                        Surface(color = MaterialTheme.colors.background) {
+                            Column {
+                                Row(verticalAlignment = Alignment.CenterVertically) {
+                                    Checkbox(checked = showOverLay, onCheckedChange = { showOverLay = !showOverLay })
+                                    Text(text = "Show Grid Overlay", style = MaterialTheme.typography.h6)
+                                }
+                                GridColumns(modifier = Modifier.padding(horizontal = LocalGridConfiguration.current.horizontalMargin))
+                            }
+                        }
                     }
                 }
             }
@@ -52,7 +67,7 @@ fun GridColumns(modifier: Modifier = Modifier) {
 
         if (columnCount < 1) return
 
-        for (i in 0..columnCount / 2)
+        for (i in 0..columnCount)
             Row(
                 horizontalArrangement = Arrangement.spacedBy(gridConfiguration.gutterWidth)
             ) {
